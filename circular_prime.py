e570d687a6b9cdce012f4daff65ce75552aca99c
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-06-04 11:28:41 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if(n%i==0):            return 0     return 1 n = int(input()) ns = list(str(n)) rs = ns[::-1] r = int(''.join(rs)) if prime(n) and prime(r):     print('circular prime') elif prime(n) and r!=prime:         print('prime but not a circular prime') # elif n!=prime: #             print('not prime') else:     print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,21 +1,17 @@
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if(n%i==0):
+           return 0
+    return 1
 n = int(input())
-c = 0
-r = 0
-x = 0
-t=n
-while(n):
-    d = n%10
-    r = r*10+d
-    n = n//10
-for i in range (1,t+1):
-    if(t%i == 0):
-        c = c+1
-for j in range (1,r+1):
-    if(r%j==0):
-        x = x+1
-if c == 2 and x == 2:
+ns = list(str(n))
+rs = ns[::-1]
+r = int(''.join(rs))
+if prime(n) and prime(r):
     print('circular prime')
-elif(c!=2 and x!=2):
+elif prime(n) and r!=prime:
+        print('prime but not a circular prime')
+# elif n!=prime:
+#             print('not prime')
+else:
     print('not prime')
-else :
-    print('prime but not a circular prime')
