25e51085f671073c20e4db250f08afd0668cf3a5
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-06-08 12:15:18 +0530

    n = int(input()) a = list(map(int, input().split())) x, y = map(int, input().split()) c = [] for i in range(len(a)):     if a[i]>=x and a[i]<=y:         c.append(a[i])     else:         continue if len(c)>0:     print(sum(c)) else:     print(-1)

---
+++ sum_of_elements_between_A_and_B.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = list(map(int, input().split()))
+x, y = map(int, input().split())
+c = []
+for i in range(len(a)):
+    if a[i]>=x and a[i]<=y:
+        c.append(a[i])
+    else:
+        continue
+if len(c)>0:
+    print(sum(c))
+else:
+    print(-1)
