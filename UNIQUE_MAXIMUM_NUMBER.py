718c4a4ac8bd921211221d4c4b07ae9f8e3af32f
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-06-16 23:47:43 +0530

    n = int(input()) a = list(map(int, input().split())) b = [] for i in a:     if i not in b:         b.append(i)     else:         continue c = [] for i in b:     k = a.count(i)     if k==1:         c.append(i)     else:         continue if len(c)>0:     print(max(c)) else:     print(-1)

---
+++ UNIQUE_MAXIMUM_NUMBER.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = []
+for i in a:
+    if i not in b:
+        b.append(i)
+    else:
+        continue
+c = []
+for i in b:
+    k = a.count(i)
+    if k==1:
+        c.append(i)
+    else:
+        continue
+if len(c)>0:
+    print(max(c))
+else:
+    print(-1)
