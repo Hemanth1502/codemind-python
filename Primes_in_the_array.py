b315835f83451cb38c4d0e1c7d9b53a778543cb4
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-06-20 23:55:59 +0530

    def prime(n):     for i in range(2, n):         if n%i==0:             return 0     return 1 n = int(input()) a = list(map(int, input().split())) b = [] for i in range(n):     if prime(a[i]) and a[i]!=1:         b.append(a[i])     else:         continue print(len(b))

---
+++ Primes_in_the_array.py
@@ -0,0 +1,14 @@
+def prime(n):
+    for i in range(2, n):
+        if n%i==0:
+            return 0
+    return 1
+n = int(input())
+a = list(map(int, input().split()))
+b = []
+for i in range(n):
+    if prime(a[i]) and a[i]!=1:
+        b.append(a[i])
+    else:
+        continue
+print(len(b))
