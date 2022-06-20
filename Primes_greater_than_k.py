a54fa241edae412884e444398762f97cb1d1b579
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-06-20 23:31:06 +0530

    def prime(n):     for i in range(2, n):         if n%i==0:             return 0     else:         return 1 n = int(input()) a = list(map(int, input().split())) k = int(input()) c = 0 for i in range(0, n):     if (prime(a[i]) and a[i]!=1) and a[i]>=k:         c = c+1     else:         continue print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,16 @@
+def prime(n):
+    for i in range(2, n):
+        if n%i==0:
+            return 0
+    else:
+        return 1
+n = int(input())
+a = list(map(int, input().split()))
+k = int(input())
+c = 0
+for i in range(0, n):
+    if (prime(a[i]) and a[i]!=1) and a[i]>=k:
+        c = c+1
+    else:
+        continue
+print(c)
