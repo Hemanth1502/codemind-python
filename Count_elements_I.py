52572e45dda8ee34037b99909cc674e8fd883d4e
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-06-21 11:18:23 +0530

    x, y = map(int, input().split()) a = list(map(int, input().split())) b = list(map(int, input().split())) c = [] for i in a:     if i not in c:         c.append(i)     else:         continue d = [] for i in b:     if i not in d:         d.append(i)     else:         continue e = [] for i in c:     if i in d:         e.append(i)     else:         continue for i in d:     if i in c:         e.append(i)     else:         continue print(len(set(e)))

---
+++ Count_elements_I.py
@@ -0,0 +1,27 @@
+x, y = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = []
+for i in a:
+    if i not in c:
+        c.append(i)
+    else:
+        continue
+d = []
+for i in b:
+    if i not in d:
+        d.append(i)
+    else:
+        continue
+e = []
+for i in c:
+    if i in d:
+        e.append(i)
+    else:
+        continue
+for i in d:
+    if i in c:
+        e.append(i)
+    else:
+        continue
+print(len(set(e)))
