5f29216945199c0710511d6b784e12d9292f6e04
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-06-30 07:44:57 +0530

    s = list(str(input()).lower()) for i in s:     if ' ' in s:         s.remove(' ')     else:         continue a = [] for i in s:     if s.count(i)==1:         a.append(i)     else:         continue g = ''.join(sorted(a)) print(g)

---
+++ Distinct_characters.py
@@ -0,0 +1,14 @@
+s = list(str(input()).lower())
+for i in s:
+    if ' ' in s:
+        s.remove(' ')
+    else:
+        continue
+a = []
+for i in s:
+    if s.count(i)==1:
+        a.append(i)
+    else:
+        continue
+g = ''.join(sorted(a))
+print(g)
