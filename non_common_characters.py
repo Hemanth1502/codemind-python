6f81aacec7c6de268ddccb878b1bdc5bb9a4dbb5
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-01 11:54:58 +0530

    s1 = list(str(input().lower())) s2 = list(str(input().lower())) s1 = list(set(s1)) s2 = list(set(s2)) a = [] for i in s1:     if ' ' in s1:         s1.remove(' ')     else:         continue for i in s2:     if ' ' in s2:         s2.remove(' ')     else:         continue for i in s1:     if i not in s2:         a.append(i)     else:         continue for i in s2:     if i not in s1:         a.append(i)     else:         continue a = ''.join(sorted(a)) print(a)

--- non_common_characters.py
+++ non_common_characters.py
@@ -1,21 +1,3 @@
-# s1 = str(input()).lower()
-# s2 = str(input()).lower()
-# # print(s1)
-# # print(s2)
-# a = []
-# for i in s2:
-#     if i not in s1:
-#         a.append(i)
-#     else:
-#         continue
-# for i in s1:
-#     if i not in s2:
-#         a.append(i)
-#     else:
-#         continue
-# b = []
-# for i in a:
-#     if i not in b:
 s1 = list(str(input().lower()))
 s2 = list(str(input().lower()))
 s1 = list(set(s1))
@@ -43,8 +25,3 @@ for i in s2:
         continue
 a = ''.join(sorted(a))
 print(a)
-# print(s1, s2)
-#         b.append(i)
-#     else:
-#         continue
-# print(''.join(sorted(b)))
