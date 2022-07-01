1775fc78e23559ef03940a271c6adf36d024153a
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-01 19:36:02 +0530

    s1 = list(str(input().lower())) s2 = list(str(input().lower())) s1 = list(set(s1)) s2 = list(set(s2)) a = [] for i in s1:     if ' ' in s1:         s1.remove(' ')     else:         continue for i in s2:     if ' ' in s2:         s2.remove(' ')     else:         continue for i in s1:     if i not in s2:         a.append(i)     else:         continue for i in s2:     if i not in s1:         a.append(i)     else:         continue print(len(a))

---
+++ Non_common_characters.py
@@ -0,0 +1,26 @@
+s1 = list(str(input().lower()))
+s2 = list(str(input().lower()))
+s1 = list(set(s1))
+s2 = list(set(s2))
+a = []
+for i in s1:
+    if ' ' in s1:
+        s1.remove(' ')
+    else:
+        continue
+for i in s2:
+    if ' ' in s2:
+        s2.remove(' ')
+    else:
+        continue
+for i in s1:
+    if i not in s2:
+        a.append(i)
+    else:
+        continue
+for i in s2:
+    if i not in s1:
+        a.append(i)
+    else:
+        continue
+print(len(a))
