cca26d3eda51f777fbba5a67d9de5845297bfab1
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-10 11:50:53 +0530

    n, m = map(int, input().split()) c = 0 for i in range(n):     a = list(map(int, input().split()))     if a==sorted(a):         c = c+1     elif a == sorted(a)[::-1]:         c = c+1     # elif a[::-1]==sorted(a):     #     c = c+1     # elif a[::-1]==sorted(a)[::-1]:     #     c = c+1 print(c)

---
+++ Count_sorted_rows_in_a_matrix.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+c = 0
+for i in range(n):
+    a = list(map(int, input().split()))
+    if a==sorted(a):
+        c = c+1
+    elif a == sorted(a)[::-1]:
+        c = c+1
+    # elif a[::-1]==sorted(a):
+    #     c = c+1
+    # elif a[::-1]==sorted(a)[::-1]:
+    #     c = c+1
+print(c)
