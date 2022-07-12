c811096192948d3d01c59c67d29715d6b6e834d8
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-12 10:08:10 +0530

    def prime(n):     for i in range(2,n):         if n%i==0:             return 0     else:         return 1 n = int(input()) a = [] for i in range(1,n+1):     if n%i==0:         a.append(i) for i in a:     if prime(i):         a.remove(i)     else:         continue print(len(a))

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,15 +1,17 @@
 def prime(n):
-    c=0
-    for i in range(2,int(n**0.5)+1):
-        if(n%i==0):
-           return 0
-    return 1
+    for i in range(2,n):
+        if n%i==0:
+            return 0
+    else:
+        return 1
 n = int(input())
-c = 0
-d = 0
-for i in range (1,n+1):
-    if (n%i==0 and prime(i) and i!=1):
-        c = c + 1
+a = []
+for i in range(1,n+1):
     if n%i==0:
-        d = d + 1
-print(abs(c-d))
+        a.append(i)
+for i in a:
+    if prime(i):
+        a.remove(i)
+    else:
+        continue
+print(len(a))
