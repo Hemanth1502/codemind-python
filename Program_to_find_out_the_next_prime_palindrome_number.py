b7eaeb52036e7d2339f9182f7345b86eae474f96
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-12 10:11:59 +0530

    def palindrome(n):     n = str(n)     if n==n[::-1]:         return 1     else:         return 0 def prime(n):     for i in range(2,n):         if n%i==0:             return 0     else:         return 1 n = int(input()) i = n+1 while(True):     if palindrome(i) and prime(i):         print(i)         break     else:         i = i+1

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,18 +1,20 @@
-def prime(n):
-    for i in range(2,int(n**0.5)+1):
-        if(n%i==0):
-           return 0
-    return 1
 def palindrome(n):
-    a = list(str(n))
-    if a == a[::-1]:
+    n = str(n)
+    if n==n[::-1]:
         return 1
     else:
         return 0
+def prime(n):
+    for i in range(2,n):
+        if n%i==0:
+            return 0
+    else:
+        return 1
 n = int(input())
-i=n+1
+i = n+1
 while(True):
-    if prime(i) and palindrome(i):
+    if palindrome(i) and prime(i):
         print(i)
         break
-    i = i + 1
+    else:
+        i = i+1
