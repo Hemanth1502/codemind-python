0a67550ca0913bbe0244b5d7c6b211407653584a
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-12 11:14:08 +0530

    def prime(n):     for i in range(2,n):         if n%i==0:             return 0     else:         return 1 t = int(input()) for i in range(t):     n = int(input())     i = n+1     while(True):         if prime(i):             p1 = i             break         else:             i = i+1     j = n     while(True):         if prime(j):             p2 = j             break         else:             j=j-1     if abs(p1-n)>abs(p2-n):         print(p2)     elif abs(p1-n)==abs(p2-n):         print(min(p1, p2))     elif abs(p1-n)<abs(p2-n):         print(p1)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,40 +1,29 @@
 def prime(n):
-    for i in range (2,n):
+    for i in range(2,n):
         if n%i==0:
             return 0
     else:
         return 1
-n = int(input())
-while(n):
-    N = int(input())
-    for i in range(1,N+1):
-        if(prime(i)):
-            l=i
-    i=N+1
-    while(1):
-        if(prime(i)):
-            j=i
+t = int(input())
+for i in range(t):
+    n = int(input())
+    i = n+1
+    while(True):
+        if prime(i):
+            p1 = i
             break
-        i+=1
-    # for i in range (2,2*N):
-    #     if prime(i):
-    #         a.append(i)
-    #     else:
-    #         continue
-    # g = list(set(sorted(a)))
-    # d = g.index(N)
-    # if abs(g[d]-g[d+1])==abs(g[d]-g[d-1]):
-    #     print(min(g[d-1],g[d+1]))
-    # if abs(g[d]-g[d+1])<abs(g[d]-g[d-1]):
-    #     print(g[d+1])
-    # if abs(g[d]-g[d+1])>abs(g[d]-g[d-1]):
-        # print(g[d-1])
-    if abs(l-N)==abs(j-N):
-        print(min(l,j))
-    elif abs(l-N)>abs(j-N):
-        print(j)
-    elif abs(l-N)<abs(j-N):
-        print(l)
-    else:
-        print(N)
-    n-=1
+        else:
+            i = i+1
+    j = n
+    while(True):
+        if prime(j):
+            p2 = j
+            break
+        else:
+            j=j-1
+    if abs(p1-n)>abs(p2-n):
+        print(p2)
+    elif abs(p1-n)==abs(p2-n):
+        print(min(p1, p2))
+    elif abs(p1-n)<abs(p2-n):
+        print(p1)
