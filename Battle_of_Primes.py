3c682fc335039e658cbb7de7feb19f1ec3d72aa0
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-12 10:15:02 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 a = int(input()) b = int(input()) i = a+b+1 while(True):     if prime(i):         print(i-(a+b))         break     else:         i+=1

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,14 +1,15 @@
 def prime(n):
     for i in range(2,int(n**0.5)+1):
-        if(n%i==0):
-           return 0
-    return 1
+        if n%i==0:
+            return 0
+    else:
+        return 1
 a = int(input())
 b = int(input())
-i = 1
+i = a+b+1
 while(True):
-    c = a+b+i
-    if prime(c):
-        print(i)
+    if prime(i):
+        print(i-(a+b))
         break
-    i = i+1
+    else:
+        i+=1
