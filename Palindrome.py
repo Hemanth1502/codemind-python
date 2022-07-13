1a9050f9a37d86a548240a9839df6b48167f4954
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-13 17:56:40 +0530

    n = int(input()) n1 = int(str(n)[::-1]) if n1==n:     print('True') else:     print('False')

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,6 @@
 n = int(input())
-temp = n
-r = 0
-while(n):
-    d = n%10
-    r = r*10 + d
-    n//=10
-if r==temp:
-    print(True)
+n1 = int(str(n)[::-1])
+if n1==n:
+    print('True')
 else:
-    print(False)
+    print('False')
