d0abdbcace6149d3b0682a9065293a4bd4d5cd39
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-13 11:05:06 +0530

    def prime(n):     for i in range(2, n):         if n%i==0:             return 0     else:         return 1 n = int(input()) # print(n) if prime(n):     print(0) else:     i = n+1     while(True):         if prime(i):             s = i             break         else:             i = i+1     j = n-1     while(True):         if prime(j):             c = j             break         else:             j = j-1     print(min(abs(s-n), abs(c-n)))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,25 +1,26 @@
 def prime(n):
-    for i in range (2,n):
+    for i in range(2, n):
         if n%i==0:
             return 0
     else:
         return 1
 n = int(input())
-for i in range (2,n+1):
-    if prime(i):
-        c = i
-i = n+1
-while(1):
-    if prime(i):
-        d = i
-        break
-    i+=1
-if abs(n-c)==abs(n-d):
-    s = min(c,d)
-elif abs(n-c)>abs(n-d):
-    s = d
-elif abs(n-c)<abs(n-d):
-    s = c
+# print(n)
+if prime(n):
+    print(0)
 else:
-    s = n
-print(abs(s-n))
+    i = n+1
+    while(True):
+        if prime(i):
+            s = i
+            break
+        else:
+            i = i+1
+    j = n-1
+    while(True):
+        if prime(j):
+            c = j
+            break
+        else:
+            j = j-1
+    print(min(abs(s-n), abs(c-n)))
