c901d62f69cb2c86d2e21a502465c6b8e65e2be0
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-13 10:36:23 +0530

    n = int(input()) a = 0 b = 1 c = a+b s = [] for i in range(n):     c = a+b     a = b     b = c     s.append(a)     if a>n:         break for i in range(len(s)):     if s[i]>n:         s.insert(i, n)     else:         continue k = s.index(n) if abs(s[k-1]-n)>abs(s[k+1]-n):     print(s[k+1]) elif abs(s[k-1]-n)==abs(s[k+1]-n):     print(s[k-1], s[k+1]) elif abs(s[k-1]-n)<abs(s[k+1]-n):     print(s[k-1])

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -2,13 +2,23 @@ n = int(input())
 a = 0
 b = 1
 c = a+b
-while(c<n):
+s = []
+for i in range(n):
+    c = a+b
     a = b
     b = c
-    c = a+b
-if(abs(c-n)>abs(b-n)):
-    print(b)
-elif(abs(c-n)==abs(b-n)):
-    print(b,c)
-else:
-    print(c)
+    s.append(a)
+    if a>n:
+        break
+for i in range(len(s)):
+    if s[i]>n:
+        s.insert(i, n)
+    else:
+        continue
+k = s.index(n)
+if abs(s[k-1]-n)>abs(s[k+1]-n):
+    print(s[k+1])
+elif abs(s[k-1]-n)==abs(s[k+1]-n):
+    print(s[k-1], s[k+1])
+elif abs(s[k-1]-n)<abs(s[k+1]-n):
+    print(s[k-1])
