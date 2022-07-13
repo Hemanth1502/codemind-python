b05ed5729857bb89654fd6391744d640efa33826
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-13 17:32:05 +0530

    a, b = map(int, input().split()) for i in range(1, b+1):     if a%i==0 and b%i==0:         c = i     else:         continue print(c)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,6 +1,7 @@
 a, b = map(int, input().split())
-k = max(a,b)
-for i in range(1,k+1):
-    if(a%i == 0 and b%i == 0):
-        GCD = i
-print(GCD)
+for i in range(1, b+1):
+    if a%i==0 and b%i==0:
+        c = i
+    else:
+        continue
+print(c)
