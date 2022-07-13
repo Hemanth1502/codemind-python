cb897b6bd06d47cf892df7ddfdb1e67bf06ec6b9
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-13 21:41:23 +0530

    n = int(input()) x =[list(map(int, input().split())) for i in range(n)] y =[list(map(int, input().split())) for i in range(n)] for i in range(n):     c = []     for j in range(n):         c.append(abs(int(x[i][j])-int(y[i][j])))     print(*c)

---
+++ Absolute_difference_of_two_matrices.py
@@ -0,0 +1,8 @@
+n = int(input())
+x =[list(map(int, input().split())) for i in range(n)]
+y =[list(map(int, input().split())) for i in range(n)]
+for i in range(n):
+    c = []
+    for j in range(n):
+        c.append(abs(int(x[i][j])-int(y[i][j])))
+    print(*c)
