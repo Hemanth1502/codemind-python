160a47903ca7eccbc178712703bc7ce8187338c7
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 07:41:55 +0530

    def palindrome(n):     x = int(str(n)[::-1])     if x==n:         return 1     else:         return 0 n = int(input()) a = list(map(int, input().split())) c = 0 for i in range(len(a)):     if palindrome(a[i]):         c = c+1 print(c)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,15 +1,13 @@
 def palindrome(n):
-    n = str(n)
-    if n==n[::-1]:
+    x = int(str(n)[::-1])
+    if x==n:
         return 1
     else:
         return 0
 n = int(input())
 a = list(map(int, input().split()))
 c = 0
-for i in range(n):
+for i in range(len(a)):
     if palindrome(a[i]):
         c = c+1
-    else:
-        continue
 print(c)
