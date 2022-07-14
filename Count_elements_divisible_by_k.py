f7311dfa1d46005426fa3a0454763d78f0b19ff9
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 07:40:19 +0530

    n, k = map(int, input().split()) a = list(map(int, input().split())) c = 0 for i in range(len(a)):     if a[i]%k==0:         c = c+1 print(c)

--- Count_elements_divisible_by_k.py
+++ Count_elements_divisible_by_k.py
@@ -1,9 +1,7 @@
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 c = 0
-for i in range(n):
+for i in range(len(a)):
     if a[i]%k==0:
         c = c+1
-    else:
-        continue
 print(c)
