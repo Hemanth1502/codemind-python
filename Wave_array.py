146533125764cfbaf901caeba83fabe1bc0a8bc7
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 08:01:33 +0530

    n = int(input()) a = list(map(int, input().split())) c = 0 for i in range(n-1):     if a[i]>a[i-1] and a[i]>a[i+1]:         c = c+1     elif a[i]<a[i-1] and a[i]<a[i+1]:         c = c+1 if c+1==n:     print('yes') else:     print('no')

--- Wave_array.py
+++ Wave_array.py
@@ -1,5 +1,4 @@
 n = int(input())
-# print(n)
 a = list(map(int, input().split()))
 c = 0
 for i in range(n-1):
@@ -7,7 +6,6 @@ for i in range(n-1):
         c = c+1
     elif a[i]<a[i-1] and a[i]<a[i+1]:
         c = c+1
-# print(c)
 if c+1==n:
     print('yes')
 else:
