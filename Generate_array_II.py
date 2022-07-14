e9fca939ac191ebfdad7361eed32109a4c515d3b
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 10:55:20 +0530

    n = int(input()) a = list(map(int, input().split())) b = [] c = [] for i in range(n):     if i%2==0:         b.append(a[i])     else:         c.append(a[i]) d = [] for j in range(len(b)):     for k in range(c[j]):         d.append(b[j]) print(*d)

--- Generate_array_II.py
+++ Generate_array_II.py
@@ -3,12 +3,10 @@ a = list(map(int, input().split()))
 b = []
 c = []
 for i in range(n):
-    if i%2==0 or i==0:
+    if i%2==0:
         b.append(a[i])
-    elif i%2!=0 :
-        c.append(a[i])
     else:
-        continue
+        c.append(a[i])
 d = []
 for j in range(len(b)):
     for k in range(c[j]):
