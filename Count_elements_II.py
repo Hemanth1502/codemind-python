da39874d3cebab7a2aa333193e7cafb186cea94b
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 10:29:29 +0530

    n1, n2 = map(int, input().split()) a1 = list(map(int, input().split())) a2 = list(map(int, input().split())) c1 = [] for i in a2:     if i not in a1:         c1.append(i) for i in a1:     if i not in a2:         c1.append(i) print(len(set(c1)))

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,28 +1,11 @@
-x, y = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-count = 0
-c = []
-for i in a:
-    if i not in c:
-        c.append(i)
-    else:
-        continue
-d = []
-for i in b:
-    if i not in d:
-        d.append(i)
-    else:
-        continue
-e = []
-for i in c:
-    if i not in d:
-        e.append(i)
-    else:
-        continue
-for i in d:
-    if i not in c:
-        e.append(i)
-    else:
-        continue
-print(len(set(e)))
+n1, n2 = map(int, input().split())
+a1 = list(map(int, input().split()))
+a2 = list(map(int, input().split()))
+c1 = []
+for i in a2:
+    if i not in a1:
+        c1.append(i)
+for i in a1:
+    if i not in a2:
+        c1.append(i)
+print(len(set(c1)))
