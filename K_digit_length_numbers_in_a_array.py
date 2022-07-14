063056d33173c80e76a7af314f6fc10525c2718f
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 07:38:09 +0530

    n, m = map(int, input().split()) a = list(map(int, input().split())) c = 0 for i in range(len(a)):     if a[i]>=0:         x = list(str(a[i]))         if len(x)==m:             c = c+1     else:         x = list(str(a[i]))         x = x[1:]         if len(x)==m:             c = c+1 print(c)

--- K_digit_length_numbers_in_a_array.py
+++ K_digit_length_numbers_in_a_array.py
@@ -1,23 +1,14 @@
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
-c = []
-for i in range(n):
+c = 0
+for i in range(len(a)):
     if a[i]>=0:
-        x = str(a[i])
-        x1 = list(x)
-        k = len(x1)
-        if k==m:
-            c.append(a[i])
-        else:
-            continue
-    elif a[i]<0:
-        x = str(a[i])[1::]
-        x1 = list(x)
-        k = len(x1)
-        if k==m:
-            c.append(a[i])
-        else:
-            continue
+        x = list(str(a[i]))
+        if len(x)==m:
+            c = c+1
     else:
-        continue
-print(len(c))
+        x = list(str(a[i]))
+        x = x[1:]
+        if len(x)==m:
+            c = c+1
+print(c)
