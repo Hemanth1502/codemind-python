96bf573cc3ac19dfaa92c5d2d8da6c5694c9c681
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 22:40:10 +0530

    n = int(input()) a = list(map(int, input().split())) b = [] for i in a:     if i not in b:         b.append(i) print(*b)

--- unique_elements.py
+++ unique_elements.py
@@ -1,9 +1,7 @@
 n = int(input())
 a = list(map(int, input().split()))
 b = []
-for i in range(len(a)):
-    if a[i] not in b:
-        b.append(a[i])
-    else:
-        continue
+for i in a:
+    if i not in b:
+        b.append(i)
 print(*b)
