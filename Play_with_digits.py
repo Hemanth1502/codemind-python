4f1c142e4a4829000b52a5cab4977a64bbc66b2e
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-14 07:50:17 +0530

    n = int(input()) a = list(map(int, input().split())) s = [] for i in range(len(a)):     x = list(str(a[i]))     for i in range(len(x)):         x[i]=int(x[i])     s.append(sum(x)) print(sum(s))

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,15 +1,9 @@
 n = int(input())
 a = list(map(int, input().split()))
-c = 0
-s = 0 
-for i in range(n):
-    if a[i]<10:
-        c = c+a[i]
-    elif a[i]>9:
-        while(a[i]):
-            d = a[i]%10
-            s = s+d
-            a[i]=a[i]//10
-    else:
-        continue
-print(s+c)
+s = []
+for i in range(len(a)):
+    x = list(str(a[i]))
+    for i in range(len(x)):
+        x[i]=int(x[i])
+    s.append(sum(x))
+print(sum(s))
