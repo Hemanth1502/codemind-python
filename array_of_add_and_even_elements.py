ff62c9fb0b7f6a3d37f4d4c4b854ff317adc4e37
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-15 07:26:18 +0530

    n = int(input()) a = list(map(int, input().split())) e = [] o = [] for i in range(len(a)):     if a[i]%2==0:         e.append(a[i])     else:         o.append(a[i]) print(*(o+e))

---
+++ array_of_add_and_even_elements.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int, input().split()))
+e = []
+o = []
+for i in range(len(a)):
+    if a[i]%2==0:
+        e.append(a[i])
+    else:
+        o.append(a[i])
+print(*(o+e))
