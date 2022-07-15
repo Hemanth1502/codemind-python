96335715f2f4de4c4bcb2fcd227c1e328e11a2a5
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-15 10:17:13 +0530

    n = int(input()) temp = n while(True):     s = 0     while(temp):         d = temp%10         s = s+d**2         temp = temp//10     if s<10:         if s ==1 or s == 7:             print(True)             break         else:             print(False)             break     else:         temp=s

--- happy_number.py
+++ happy_number.py
@@ -1,13 +1,17 @@
 n = int(input())
-add = 0
-while(add != 1 and add!=4):
-    add = 0
-    while(n):
-        d = n%10
-        add = add+d**2
-        n = n//10
-    n = add
-if add==1 or add==7:
-    print("True")
-else:
-    print("False")
+temp = n
+while(True):
+    s = 0
+    while(temp):
+        d = temp%10
+        s = s+d**2
+        temp = temp//10
+    if s<10:
+        if s ==1 or s == 7:
+            print(True)
+            break
+        else:
+            print(False)
+            break
+    else:
+        temp=s
