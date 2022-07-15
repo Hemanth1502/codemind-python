8cbd51ef8cb0e65ce65988ad043469c8610060bd
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-15 23:05:43 +0530

    n = int(input()) a = list(map(int, input().split())) for i in range(len(a)):     if a[i]%2!=0:         c = i print(c)

--- last_odd_number_index_position.py
+++ last_odd_number_index_position.py
@@ -1,9 +1,6 @@
 n = int(input())
 a = list(map(int, input().split()))
-for i in range(n):
-    c = 0
-    if a[i]%2==1:
-        c = c + a[i]
-    else:
-        continue
-print(i)
+for i in range(len(a)):
+    if a[i]%2!=0:
+        c = i
+print(c)
