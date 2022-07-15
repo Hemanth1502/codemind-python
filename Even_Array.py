7d22a21ed248dba7d684d4016e0dd1268fda5b97
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-15 07:35:38 +0530

    n = int(input()) a = list(map(int, input().split())) c = 0 for i in a:     if i%2==0:         c = c+1 if c == n:     print(True) else:     print(False)

--- Even_Array.py
+++ Even_Array.py
@@ -1,12 +1,10 @@
 n = int(input())
 a = list(map(int, input().split()))
-c = 0
-for i in range(len(a)):
-    if a[i]==0 or a[i]%2==0:
+c = 0 
+for i in a:
+    if i%2==0:
         c = c+1
-    else:
-        continue
-if c==len(a):
+if c == n:
     print(True)
 else:
     print(False)
