0c73e52259d2f15f0f83b9f375c35a278eebbb97
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-15 22:55:45 +0530

    n = int(input()) a = list(map(int, input().split())) c = 0 for i in range(len(a)):     if (a[i])%2!=0 and (i)%2!=0:         c = 0     else:         if (a[i])%2!=0 and i%2==0:             c = c+1             break         else:             continue if c>0:     print(False) else:     print(True)

--- Strictly_ODD.py
+++ Strictly_ODD.py
@@ -1,21 +1,16 @@
-def odd(n):
-    if n%2!=0 and n!=0:
-        return 1
-    else:
-        return 0
 n = int(input())
 a = list(map(int, input().split()))
 c = 0
 for i in range(len(a)):
-    if odd(a[i]) and odd(i):
+    if (a[i])%2!=0 and (i)%2!=0:
         c = 0
     else:
-        if odd(a[i]) and i!=odd:
-            c = 1
+        if (a[i])%2!=0 and i%2==0:
+            c = c+1
             break
         else:
             continue
-if c==1:
+if c>0:
     print(False)
 else:
     print(True)
