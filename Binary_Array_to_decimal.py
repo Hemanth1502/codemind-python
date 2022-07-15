ba564a1c19ebc50dde6855c765106c064159f584
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-15 07:40:03 +0530

    n = int(input()) a = list(map(int, input().split())) c = [] for i in range(len(a)):     c.append((a[i])*(2**(len(a)-(i+1)))) print(sum(c))

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -1,11 +1,6 @@
 n = int(input())
 a = list(map(int, input().split()))
-k = len(a)-1
-c = 0
+c = []
 for i in range(len(a)):
-    if a[i]==1:
-        c = c + (a[i]*2)**(k) #8 + 4 + 
-        k = k-1
-    else:
-        k = k-1
-print(c)
+    c.append((a[i])*(2**(len(a)-(i+1))))
+print(sum(c))
