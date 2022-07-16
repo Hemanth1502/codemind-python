0347c88616d39a5b0eb24f4a6ab09d391d70ef19
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-16 11:19:53 +0530

    s = list(str(input()).lower()) for i in s:     if i==' ':         s.remove(i) a = [] for i in s:     if s.count(i)==1:         a.append(i) print(len(sorted(set(a))))

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,14 +1,9 @@
 s = list(str(input()).lower())
 for i in s:
-    if ' ' in s:
-        s.remove(' ')
-    else:
-        continue
+    if i==' ':
+        s.remove(i)
 a = []
 for i in s:
     if s.count(i)==1:
         a.append(i)
-    else:
-        continue
-g = ''.join(sorted(a))
-print(len(g))
+print(len(sorted(set(a))))
