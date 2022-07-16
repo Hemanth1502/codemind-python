119ea4afe45da31d1e2f0b49bbdea792921ab481
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-16 11:20:57 +0530

    s = list(str(input()).lower()) for i in s:     if i==' ':         s.remove(i) a = [] for i in s:     if s.count(i)==1:         a.append(i) if len(a)>0:     print(a[0]) else:     print(-1)

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,17 +1,12 @@
 s = list(str(input()).lower())
 for i in s:
-    if ' ' in s:
-        s.remove(' ')
-    else:
-        continue
+    if i==' ':
+        s.remove(i)
 a = []
 for i in s:
     if s.count(i)==1:
         a.append(i)
-    else:
-        continue
-g = ''.join(a)
-if len(g)>0:
-    print(g[0])
+if len(a)>0:
+    print(a[0])
 else:
     print(-1)
