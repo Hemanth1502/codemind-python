98920f0fb190e26f4e18ecfbbe4c37f33fb3d8b5
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-07-16 11:22:13 +0530

    s = list(str(input()).lower()) for i in s:     if i==' ':         s.remove(i) print(len(sorted(set(s))))

--- Count_Distinct_characters.py
+++ Count_Distinct_characters.py
@@ -1,13 +1,5 @@
 s = list(str(input()).lower())
 for i in s:
-    if ' ' in s:
-        s.remove(' ')
-    else:
-        continue
-a = []
-for i in s:
-    if i not in a:
-        a.append(i)
-    else:
-        continue
-print(len(a))
+    if i==' ':
+        s.remove(i)
+print(len(sorted(set(s))))
