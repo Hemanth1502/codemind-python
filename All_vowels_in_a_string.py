292126da0126bf62e020788ebbaf7599d716f68c
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-10-24 13:03:30 +0530

    s=str(input()) a=[] b=[] for i in s:     if i in 'aeiou':         a.append(i)     if i in 'AEIOU':         b.append(i) if len(set(a))>=5:     print(True) elif len(set(b))>=5:     print(True) else:     print(False)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,13 +1,14 @@
-s = input()
-a = ['a','e','i','o','u']
-c = 0
+s=str(input())
+a=[]
+b=[]
 for i in s:
-    if i in a:
-        c = c+1
-        a.remove(i)
-    else:
-        continue
-if c>=5:
+    if i in 'aeiou':
+        a.append(i)
+    if i in 'AEIOU':
+        b.append(i)
+if len(set(a))>=5:
+    print(True)
+elif len(set(b))>=5:
     print(True)
 else:
     print(False)
