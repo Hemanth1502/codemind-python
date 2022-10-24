8d5be701e928924cfb042723508b077bca4dcb8e
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-10-24 12:02:09 +0530

    s=(str(input()).lower()).split() c=0 for i in s:     if i==i[::-1]:         c=c+1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,8 +1,6 @@
-s = (str(input()).lower()).split()
-c = 0
-for i in range(len(s)):
-    if s[i]==s[i][::-1]:
-        c = c+1
-    else:
-        continue
+s=(str(input()).lower()).split()
+c=0
+for i in s:
+    if i==i[::-1]:
+        c=c+1
 print(c)
