8ee4f681c72533821586f3eccdd4f040dd849198
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-10-24 12:10:38 +0530

    s1=str(input()).lower() s2=str(input()).lower() if sorted(s1)==sorted(s2):     print('True') else:     print('False')

--- Anagrams.py
+++ Anagrams.py
@@ -1,12 +1,6 @@
-s1 = str(input()).lower()
-s2 = str(input()).lower()
-c = 0
-for i in s1:
-    if i in s2:
-        c = c+1
-    else:
-        continue
-if c==len(s1):
-    print(True)
+s1=str(input()).lower()
+s2=str(input()).lower()
+if sorted(s1)==sorted(s2):
+    print('True')
 else:
-    print(False)
+    print('False')
