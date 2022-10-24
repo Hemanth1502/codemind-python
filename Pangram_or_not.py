039414727d154cbb92c94f44ac443ebc318adb06
Hemanth1502 <98727448+Hemanth1502@users.noreply.github.com>
2022-10-24 12:48:34 +0530

    s=list(str(input()).lower()) for i in s:     if i==' ':         s.remove(i) if len(set(s))==26:     print(True) else:     print(False)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,12 +1,8 @@
-s = str(input()).lower()
-s = list(s)
+s=list(str(input()).lower())
 for i in s:
     if i==' ':
         s.remove(i)
-    else:
-        continue
-s = ''.join(set(s))
-if len(s)==26:
+if len(set(s))==26:
     print(True)
 else:
     print(False)
